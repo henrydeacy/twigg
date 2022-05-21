d0e2077968fdf6669b274ab8ca3abf63ca07ded6
henrydtheodo <henryd@theodo.co.uk>
2022-05-21 17:03:25 +0100

    add react navigator

--- App.tsx
+++ App.tsx
@@ -1,7 +1,21 @@
+import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Welcome" component={Welcome} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function Welcome() {
   return (
     <View style={styles.container}>
       <Text>Welcome to toda!</Text>
