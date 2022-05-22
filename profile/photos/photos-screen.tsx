458745b0ca6d3e3a83aced18e9419b0db062484e
henrydtheodo <henryd@theodo.co.uk>
2022-05-22 16:16:28 +0100

    add photo uploader

--- profile/photos/photos-screen.tsx
+++ profile/photos/photos-screen.tsx
@@ -1,12 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Text, StyleSheet, View } from "react-native";
+import PhotoUpload from "react-native-photo-upload";
 
 export function Photos({ navigation }: any) {
   return (
     <View style={styles.container}>
       <Text>Add some photos of yourself here:</Text>
       <StatusBar style="auto" />
+      <PhotoUpload>
+        <Image
+          source={{
+            uri: "https://www.sparklabs.com/forum/styles/comboot/theme/images/default_avatar.jpg",
+          }}
+        />
+      </PhotoUpload>
     </View>
   );
 }
