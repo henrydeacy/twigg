7bbc3ec248ecd37aed14eb1266778a6c4b51b552
henrydtheodo <90182308+henrydtheodo@users.noreply.github.com>
2022-06-12 14:16:31 +0100

    align delete button

--- profile/photos/photo-component.tsx
+++ profile/photos/photo-component.tsx
@@ -13,21 +13,20 @@ export function PhotoComponent({ navigation }: any) {
     const [imagePressed, setImagePressed] = useState(false)
     const width = imagePressed ? 200: 150
   return (
-    <><TouchableOpacity onPress={()=>{setImagePressed(!imagePressed)}}><Image
+    <View style={styles.container}><TouchableOpacity onPress={()=>{setImagePressed(!imagePressed)}}><Image
       style={styles.photo}
       source={{
         uri: "https://i0.wp.com/post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/03/GettyImages-1092658864_hero-1024x575.jpg?w=1155&h=1528",
       }}
     /></TouchableOpacity>{imagePressed && <View style={styles.delete}>
     <TouchableOpacity><Text style={styles.deleteText}>Delete</Text></TouchableOpacity>
-  </View>}</>
+  </View>}</View>
   );
 }
 
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 50,
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
