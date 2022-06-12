5c58294f080c18a3b8c446274bafbf56c99db7cc
henrydtheodo <90182308+henrydtheodo@users.noreply.github.com>
2022-06-12 14:15:18 +0100

    create delete button

--- profile/photos/photo-component.tsx
+++ profile/photos/photo-component.tsx
@@ -5,6 +5,7 @@ import {
   StyleSheet,
   Image,
   TouchableOpacity,
+  View,
 
 } from "react-native";
 
@@ -12,13 +13,14 @@ export function PhotoComponent({ navigation }: any) {
     const [imagePressed, setImagePressed] = useState(false)
     const width = imagePressed ? 200: 150
   return (
-    <TouchableOpacity onPress={()=>{setImagePressed(!imagePressed)}}><Image
+    <><TouchableOpacity onPress={()=>{setImagePressed(!imagePressed)}}><Image
       style={styles.photo}
-      width={width}
       source={{
         uri: "https://i0.wp.com/post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/03/GettyImages-1092658864_hero-1024x575.jpg?w=1155&h=1528",
       }}
-    /></TouchableOpacity>
+    /></TouchableOpacity>{imagePressed && <View style={styles.delete}>
+    <TouchableOpacity><Text style={styles.deleteText}>Delete</Text></TouchableOpacity>
+  </View>}</>
   );
 }
 
@@ -32,10 +34,26 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   photo: {
+    width: 150,
     height: 150,
     borderRadius: 4,
     margin: 10,
     alignItems: "center",
     justifyContent: "center",
   },
+  delete: {
+    marginBottom: 30,
+    padding: 5,
+    width: 50,
+    height: 30,
+    fontSize: 20,
+    fontStyle: { color: "#fff" },
+    backgroundColor: "#225555",
+    borderRadius: 8,
+    display: 'flex',
+    alignItems: "center"
+  },
+ deleteText:
+  { color:"#fff",
+    textAlignVertical: 'center'}
 });
