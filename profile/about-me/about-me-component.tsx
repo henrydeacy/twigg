7aa50bc9e3ea093e3cd4d7ef2a6ea068b21e9a83
henrydtheodo <90182308+henrydtheodo@users.noreply.github.com>
2022-06-19 17:10:12 +0100

    add dropdown menu

--- profile/about-me/about-me-component.tsx
+++ profile/about-me/about-me-component.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import {
   StyleSheet,
   View,
+  Text,
   TextInput,
   TouchableOpacity,
 } from "react-native";
@@ -18,17 +19,18 @@ export function AboutMeComponent (props: AboutMeComponentProps) {
     const [isOpen, setIsOpen] = useState(false)
     const openMenu = () => {setIsOpen(true)}
     const closeMenu = () => {setIsOpen(false)}
+    const [selectedQuestion, setSelectedQuestion] = useState('Select a question')
   return (
     <View style={styles.container}>
         <Menu
-          visible={true}
+        style={styles.questionBar}
+          visible={isOpen}
           onDismiss={closeMenu}
-          anchor={<TouchableOpacity onPress={
-           openMenu}/>}>
-          <Menu.Item onPress={() => {}} title="Item 1" />
-          <Menu.Item onPress={() => {}} title="Item 2" />
-          <Divider />
-          <Menu.Item onPress={() => {}} title="Item 3" />
+          anchor={<TouchableOpacity style={styles.questionBar}onPress={
+           openMenu}><Text>{selectedQuestion}</Text></TouchableOpacity>}>
+          <Menu.Item style={styles.questionBar} onPress={() => {setSelectedQuestion("Question1"); closeMenu()}} title="Question1" />
+          <Menu.Item style={styles.questionBar} onPress={() => {setSelectedQuestion("Question2"); closeMenu()}} title="Question2" />
+          <Menu.Item style={styles.questionBar} onPress={() => {setSelectedQuestion("Question3"); closeMenu()}} title="Question3" />
         </Menu>
       <TextInput multiline={true} style={styles.aboutMe}
       placeholder="Add your answer here"/>
@@ -43,31 +45,24 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     borderRadius: 4,
-    margin: 5
+    margin: 5,
   },
   aboutMe: {
     width: 360,
-    height: 360,
+    height: 320,
     borderRadius: 4,
     margin: 10,
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: colors.white
   },
-  delete: {
-    marginBottom: 30,
-    padding: 5,
-    width: 50,
-    height: 30,
-    fontSize: 20,
-    fontStyle: { color: colors.white },
-    backgroundColor: colors.blue,
-    borderRadius: 8,
-    display: 'flex',
-    alignItems: "center",
-    position: "absolute"
-  },
- deleteText:
-  { color: colors.white,
-    textAlignVertical: 'center'}
+questionBar: {
+  backgroundColor: colors.white,
+  width: 360,
+  height: 30,
+  margin: 10,
+  borderRadius: 4,
+  display: "flex",
+  alignItems: "center",
+}
 });
