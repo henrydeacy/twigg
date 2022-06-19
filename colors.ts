e2775e7e67896f85a8b7a4b1061b4c18b185ccc7
henrydtheodo <90182308+henrydtheodo@users.noreply.github.com>
2022-06-19 16:52:24 +0100

    add dropdown menu

--- colors.ts
+++ colors.ts
@@ -1,1 +1,1 @@
-export const colors = {black: "#0000000", blue: "#afaaf8", green: "#c4f8aa", pink: "#f8aae3",  white: "#ffffff", yellow: '#f3f8aa'}
+export const colors = {black: "#000000", blue: "#afaaf8", green: "#c4f8aa", pink: "#f8aae3",  white: "#ffffff", yellow: '#f3f8aa'}
